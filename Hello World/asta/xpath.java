5306806f02012b2843a91fba88baf10727fb9ce1
Bindy Dijurido <filip.skwierczynski@gmail.com>
2017-06-28 16:12:41 +0200

    small refactoring

--- Hello World/asta/xpath.java
+++ Hello World/asta/xpath.java
@@ -2,19 +2,19 @@ package asta;
 
 import org.openqa.selenium.By;
 
-public class xpath{
+public class xpath {
 
     private static final String ROOT = "html/body/div[1]/div/div[2]/div[1]/form/div[1]/";
     private static final String FORM = "div[%s]/div/div/div/input";
     private static final String BUTTON = "div[%s]/div/div/div/span/button";
 
-    public xpath(){
+    public xpath() {
     }
 
     public static By getForm(String element) {
       return (By) By.xpath(ROOT + String.format(FORM, element));
     }
-    public static By getButton(String element){
+    public static By getButton(String element) {
         return (By) By.xpath(ROOT + String.format(BUTTON, element));
     }
 
